895937cdf668e46543972782c184d7aa26f02b99
Nadeem Ali Mohammed <nadeemali.dev@gmail.com>
2025-07-10 23:56:57 -0400

    Setup deployment

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/fiscalx">
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
